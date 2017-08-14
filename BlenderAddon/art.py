f913fc73c3f1c54fffceb2376ff22dade73ce594
Tanda <lodetanda@gmail.com>
2017-08-14 00:48:25 -0300

    Addon for Blender

--- BlenderAddon/art.py
+++ BlenderAddon/art.py
@@ -1,26 +1,48 @@
-import bpy
-from bpy.props import IntProperty, PointerProperty
-from bpy.types import PropertyGroup, Operator, Panel
+# ***** BEGIN GPL LICENSE BLOCK *****
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# The Original Code is Copyright (C) 2017 Marcelo "Tanda" Cerviño. http://lodetanda.blogspot.com/ 
+# All rights reserved.
+#
+# Contributor(s):
+#
+# ***** END GPL LICENCE BLOCK *****
 
 bl_info = { 
-    "name": "ART -Aproximate Render Time Calculator-",
+    "name": "ART -Approximate Render Time Calculator-",
     "author": "Marcelo 'Tanda' Cerviño",
     "version": (1, 0),
     "blender": (2, 78),
-    "location": "Render > ART -Aprox Render Time-",
-    "description": "Aproximate Render Time Calculator",
+    "location": "Render > ART -Approx Render Time-",
+    "description": "Approximate Render Time Calculator",
     "warning": "",
     "wiki_url": "https://github.com/eLeDeTe-LoDeTanda/ART",
     "tracker_url": "https://github.com/eLeDeTe-LoDeTanda/ART/issues",
     "category": "Render",
 }
 
+import bpy
+from bpy.props import IntProperty, PointerProperty
+from bpy.types import PropertyGroup, Operator, Panel
 
 ###########################################################
 
 
 class artPanel(Panel):
-    bl_label = "ART -Aprox Render Time-"
+    bl_label = "ART -Approx Render Time-"
     bl_idname = "RENDER_PT_ART"
     bl_space_type = 'PROPERTIES'
     bl_region_type = 'WINDOW'
